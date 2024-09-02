765c19bf9542b3bf03b3a4dcbad05849a442bc51
kimgeonjung <rjs2021@gmail.com>
2024-09-02 19:08:27 +0900

    first commit

---
+++ js/about.5e2c5261.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_vue"]=self["webpackChunkmy_vue"]||[]).push([[594],{603:function(u,n,e){e.r(n),e.d(n,{default:function(){return l}});var t=e(641);const a={class:"about"},r=(0,t.Lk)("h1",null,"This is an about page",-1),s=[r];function c(u,n){return(0,t.uX)(),(0,t.CE)("div",a,s)}var i=e(6262);const o={},f=(0,i.A)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.5e2c5261.js.map
